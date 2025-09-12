19c94b4dbf9e46d49cb44f47a4f8be7d88864d16
aswin-haridas <aswinharidas0@gmail.com>
2025-09-13 01:36:30 +0530

    final commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,20 @@
-'use client';
-
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+export const metadata = {
+  title: "hmm",
+  icons: {
+    icon: "/title.svg",
+  },
+};
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export default function RootLayout({
@@ -20,7 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
